e2379cbfb991ed155ede9df353412d36a255dd26
Le Tai <tailv@gmail.com>
2021-05-05 11:34:05 +0700

    20210505:113405

--- dump.js
+++ dump.js
@@ -1,38 +1,30 @@
-var http = require('http');
+let http = require('http')
+let GPORT = 8080
 
-var LISTEN_ON_PORT = 3000;
-
-function toTitleCase(str) {
-    return str.replace(/[a-z]*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+function toTitleCase (str) {
+  return str.replace(/[a-z]*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })
 }
 
 http.createServer(function (req, res) {
-    var body;
+  let body = ''
+  req.on('data', function (chunk) { body += chunk })
+  req.on('end', function () {
+    console.log(req.method + ' ' + req.url + ' HTTP/' + req.httpVersion)
 
-    body = '';
-    req.on('data', function(chunk) {
-        body += chunk;
-    });
+    // Headers
+    for (prop in req.headers) {
+      console.log(toTitleCase(prop) + ': ' + req.headers[prop])
+    }
 
-    req.on('end', function() {
-        console.log(req.method + ' ' + req.url + ' HTTP/' + req.httpVersion);
+    // Body
+    if (body.length > 0) {
+      console.log('\n' + body + '\n')
+    }
 
-        for (prop in req.headers) {
-            console.log(toTitleCase(prop) + ': ' + req.headers[prop]);
-        }
+    res.writeHead(200)
+    res.end()
+  })
 
-        if (body.length > 0) {
-            console.log('\n' + body);
-        }
-        console.log('');
-
-        res.writeHead(200);
-        res.end();
-    });
-
-    req.on('err', function(err) {
-        console.error(err);
-    });
-}).listen(LISTEN_ON_PORT, function () {
-    console.log('Server listening on port ' + LISTEN_ON_PORT);
-});
+  // Error
+  req.on('err', function (err) { console.error(err) })
+}).listen(GPORT, function () { console.log('Server listening on port ' + GPORT) })
